f6b47b80a428a39c515b05cfa0f9c6a19d30f30a
Luis Guaiquirian <luisdavidguaiquirian@gmail.com>
2023-04-02 17:24:21 -0400

    Adding new gallery section

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,12 @@
 "use strict";
+const list = document.querySelector(".gallery-carousel__img-container--list");
+const imgs = Array.from(list.children);
+
+const nextButton = document.querySelector(".gallery-carousel__btn--right");
+const prevButton = document.querySelector(".gallery-carousel__btn--left");
+const carouselNav = document.querySelector(".gallery-carousel__dots");
+const dots = Array.from(carouselNav.children);
+
+// img width
+const imgWidth = imgs[0].getBoundingClientRect().width;
+console.log(imgWidth);
